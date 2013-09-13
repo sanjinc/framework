0e3c85ef0df55f5b8dd8e8254d9ef015b4b1b6a5
Sven <sven@webiny.com>
2013-09-13 15:49:42 +0200

    Finished Router components, still need to write the documentation.

---
+++ library/Webiny/Component/Router/RouterException.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Webiny Framework (http://www.webiny.com/framework)
+ *
+ * @link      http://www.webiny.com/wf-snv for the canonical source repository
+ * @copyright Copyright (c) 2009-2013 Webiny LTD. (http://www.webiny.com)
+ * @license   http://www.webiny.com/framework/license
+ */
+
+namespace Webiny\Component\Router;
+
+use Webiny\Component\StdLib\Exception\ExceptionAbstract;
+
+/**
+ * Exception class for Router component.
+ *
+ * @package		 Webiny\Component\Router
+ */
+ 
+class RouterException extends ExceptionAbstract{}
